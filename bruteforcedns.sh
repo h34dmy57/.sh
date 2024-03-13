b1eff2fc409c049d46424ae3300b9b2020cd9815
Ícaro Costa <babayagadeveloper@gmail.com>
2024-03-13 13:14:08 -0300

    correção

--- bruteforcedns.sh
+++ bruteforcedns.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $1 == ""]
+if [ $1 == ""] 2> /dev/null
 then
         echo "Modo de uso: $0 site.com.br"
-        echo "Para usar a ferramenta voce precisa de um arquivo com o nome 'list.txt' com uma lista de possiveis subdomains"
+        echo "Para usar a ferramenta voce precisa de um arquivo com o nome 'list' com uma lista de possiveis subdomains"
 else
-        for palavra in $(cat list.txt);
+        for subdomain in $(cat list.txt);
 do
-        host $palavra.$1 | grep -v "NXDOMAIN";
+        host $subdomain.$1 | grep -v "NXDOMAIN";
 done
 fi
