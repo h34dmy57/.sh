839a4ac216622ed65d7fe9fcd2f86be2fe3bad1f
Ícaro Costa <babayagadeveloper@gmail.com>
2024-03-13 11:51:12 -0300

    teste

--- pingsweep.sh
+++ pingsweep.sh
@@ -4,9 +4,7 @@ if [$1 == ""]
 then
         echo "Modo de uso: $0 192.168.0"
 else
-
-echo "Esse script realiza verifica quais IPs respondem ao ping num /24"
-echo "Os seguintes IPs estao respondendo esta requisicao:"
+echo "Os seguintes IPs estao respondendo ao ping:"
 for ip in {1..254};
 do
         ping -c 1 -w 1 $1.$ip | grep "64 bytes" | cut -d : -f1;
