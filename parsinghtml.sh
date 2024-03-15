cdfd0b7ec4c8f975df45068765f61beeca5706a1
Ícaro Costa <babayagadeveloper@gmail.com>
2024-03-15 06:24:32 -0300

    Correção

--- parsinghtml.sh
+++ parsinghtml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == " " ]
+if [$1 == ""]
 then
         echo "HTML PARSING"
         echo "Modo de uso: $0 google.com"
@@ -10,7 +10,7 @@ url=$1
 output_file=index.html
 wget -O $output_file $url > /dev/null 2>&1
 
-if [ $? -eq 0 ];
+if [$? -eq 0];
 then
         echo "Vefificando subdomains..."
         grep href index.html | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > list
